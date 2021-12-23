a6d1d86e0268732cf663ccb8892ed3e28ba06926
talha <syedtalha.bukhari@pointivo.com>
2021-12-23 22:18:05 +0500

    [sc-10646] Added dataset loader for training/testing

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,11 +1,13 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 from .scannet import ScannetDetectionDataset, ScannetDatasetConfig
 from .sunrgbd import SunrgbdDetectionDataset, SunrgbdDatasetConfig
+from .telecomtower import TelecomTowerDetectionDataset, TelecomTowerPredictionDataset, TelecomTowerDatasetConfig
 
 
 DATASET_FUNCTIONS = {
     "scannet": [ScannetDetectionDataset, ScannetDatasetConfig],
     "sunrgbd": [SunrgbdDetectionDataset, SunrgbdDatasetConfig],
+    "telecomtower": [TelecomTowerDetectionDataset, TelecomTowerDatasetConfig],
 }
 
 
